d10bc3b8d3d3d0d7cbd05203df0dad6ddf46d069
Diederik van Liere <dvanliere@wikimedia.org>
2012-08-21 18:17:15 -0400

    Added extra documentation, additional error handling and compiles now on OSX.

--- collector.c
+++ collector.c
@@ -11,6 +11,10 @@
  License: public domain (as if there's something to protect ;-)
 
  $Id: collector.c 12389 2006-01-04 11:21:01Z midom $
+
+ The general workflow of webstatscollector is as follows:
+ squids -> udp2log -> filter -> log2udp -> collector -> files
+
 */
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -24,9 +28,12 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <db.h>
 #include <pthread.h>
 #include "collector.h"
+#include <stdlib.h>
+
 
 void hup();
 void alarmed();
@@ -44,11 +51,10 @@ void increaseStatistics(DB *, char *, struct wcstats * );
 int needdump=0;
 
 int main(int ac, char **av) {
-
 	ssize_t l;
 	char buf[2000];
 	int r;
-
+	int retval;
 
 	/* Socket variables */
 	int s, exp;
@@ -56,15 +62,25 @@ int main(int ac, char **av) {
 	int port;
 	struct sockaddr_in me, them;
 	socklen_t sl;
-	
+
 	struct pollfd fds[2];
-	
+
 	/*Initialization*/{
+		#ifdef __linux___
+		printf("initialization\n");
+		retval = daemon(0, 1);
+		if(retval  == -1) {
+			fprintf(stderr,"Failed to daemonize.\n");
+			exit(EXIT_FAILURE);
+		}
+		#endif
+		setuid(65534);
+		setgid(65534);
 		port=3815;
 		bzero(&me,sizeof(me));
 		me.sin_family= AF_INET;
 		me.sin_port=htons(port);
-	
+		inet_aton("127.0.0.1", &me.sin_addr);
 		s=socket(AF_INET,SOCK_DGRAM,0);
 		bind(s,(struct sockaddr *)&me,sizeof(me));
 
@@ -83,45 +99,51 @@ int main(int ac, char **av) {
 
 		db=initEmptyDB();
 		aggr=initEmptyDB();
-		
+
 		signal(SIGALRM,alarmed);
 		signal(SIGHUP,hup);
 		signal(SIGINT,die);
 		signal(SIGTERM,die);
 		signal(SIGCHLD,child);
 		signal(SIGUSR1,truncatedb);
-		
+
 		/* Schedule the dumps */
 		alarm(PERIOD-(time(NULL)%PERIOD));
 	}
 	/* Loop! loop! loop! */
+	printf("Looping\n");
 	for(;;) {
+		printf("1: here we are again\n");
 		r=poll(fds,2,-1);
-		
 		if (needdump)
 			produceDump();
-		
+
 		/* Process incoming UDP queue */
-		while(( fds[0].revents & POLLIN ) && 
+		while(( fds[0].revents & POLLIN ) &&
 			((l=recvfrom(s,&buf,1500,0,NULL,NULL))!=-1)) {
 				if (l==EAGAIN)
 					break;
 				handleMessage((char *)&buf,l);
 			}
-				
+
 		/* Process incoming TCP queue - for testing data collection only */
-		while((fds[1].revents & POLLIN ) && 
+		while((fds[1].revents & POLLIN ) &&
 			((r=accept(exp,(struct sockaddr *)&them,&sl))!=-1)) {
 				if (r==EWOULDBLOCK)
 					break;
 				handleConnection(r);
 		}
 	}
-
+	printf("closing the db's\n");
 	db->close(db, 0);
 	aggr->close(aggr, 0);
 
 	return(0);
+}
+
+void print_error(int error){
+	char *msg = strerror(error);
+	fprintf(stderr,"Encountered error:%s\n", msg);
 }
 
 /* Decides what to do with incoming UDP message */
@@ -131,32 +153,44 @@ void handleMessage(char *buf,ssize_t l) {
 	char title[1024];
 	char keytext[1200];
 	int r;
-	
+
 	unsigned long long serial;
-	
+
 	struct wcstats incoming;
 	/* project count bytesize page */
-	const char msgformat[]="%llu %127s %llu %llu %1023[^\n]"; 
-	
+
+	const char msgformat[]="%llu %127s %llu %llu %1023[^\n]";
+
+	/* field 1: unsigned long serial
+	 * field 2: project title with a max of 127 bytes excluding the '\0'
+	 * field 3: unsigned long count, by default 1
+	 * field 4: unsigned long size of http request
+	 * field 5: page title with a max of 1023 bytes excluding the '\0'
+	 */
+	printf("3: Handling the message\n");
+
 	buf[l]=0;
 	pp=buf;
-	
+
 	while((p=strsep(&pp,"\r\n"))) {
+
 		if (p[0]=='\0')
 			continue;
 		if (!strcmp("-truncate",p)) {
 			truncatedb();
 			return;
 		}
+		printf("Got the message: %s.\n", p);
 		bzero(&incoming,sizeof(incoming));
 		r=sscanf(p,msgformat,&serial,(char *)&project,
 			&incoming.wc_count,
 			&incoming.wc_bytes,
 			(char *)&title);
+		printf("Number of fields: %d\n", r);
 		if (r<5)
 			continue;
-		snprintf(keytext,1499,"%s:%s",project,title);
-		
+		snprintf(keytext,1199,"%s:%s",project,title);
+
 		increaseStatistics(db,keytext,&incoming);
 		increaseStatistics(aggr,project,&incoming);
 	}
@@ -165,30 +199,35 @@ void handleMessage(char *buf,ssize_t l) {
 /* Create empty database object - creates file-system backed anonymous BDB handle */
 DB * initEmptyDB() {
 	/* Do note - this isn't using global 'db' object */
+	int retval;
 	DB *db;
-	db_create(&db,NULL,0);
-	db->open(db,NULL,".temp.db",NULL,DB_BTREE,DB_CREATE|DB_TRUNCATE,0);
+	fprintf(stderr,"Creating db's\n");
+	retval = db_create(&db,NULL,0);
+	db->set_cachesize(db, 0, 1024*1024*1024, 0);
+	retval =db->open(db,NULL,".temp.db",NULL,DB_BTREE,DB_CREATE|DB_TRUNCATE,0);
+	if (retval==-1){
+		print_error(retval);
+	}
 	unlink(".temp.db");
-	return db;	
+	return db;
 }
 
 /* Bump up statistics in specified DB for specified key */
 void increaseStatistics(DB *db, char *keytext, struct wcstats *incoming ) {
 	struct wcstats *old;
 	DBT key,data;
-		
+
 	bzero(&key,sizeof(key));
 	key.data=keytext;
 	key.size=strlen(keytext)+1;
-	
+
 	bzero(&data,sizeof(data));
-		
 	if (db->get(db,NULL,&key,&data,0)==0) {
 		/* Update old stuff */
 		old=data.data;
 		old->wc_count   += incoming->wc_count;
 		old->wc_bytes   += incoming->wc_bytes;
-		db->put(db,NULL,&key,&data,0);	
+		db->put(db,NULL,&key,&data,0);
 	} else {
 		/* Put in fresh data */
 		data.data=incoming;
@@ -208,7 +247,7 @@ void statsDumper(struct dumperjob * job) {
 	snprintf(filename,1000,"%s-%04d%02d%02d-%02d%02d%02d",
 		job->prefix, dumptime.tm_year+1900, dumptime.tm_mon+1, dumptime.tm_mday,
 		dumptime.tm_hour, dumptime.tm_min, dumptime.tm_sec);
-	
+
 	snprintf(tfilename,1010,"%s.tmp",filename);
 	outfile=fopen(tfilename,"a");
 	if (outfile==NULL) {
@@ -229,34 +268,37 @@ void produceDump() {
 			2.2) Destroy old databases
 		3) Set the alarm to next PERIOD-thetime%PERIOD - this will position the alarm at next nice period.
 	*/
-	
-	pthread_t dumper,aggrdumper;
 	static struct dumperjob dumperJob, aggrDumperJob;
 	DB *olddb,*oldaggr;
+
+	pthread_t dumper,aggrdumper;
 	time_t dumptime=0;
-	
+
 	time(&dumptime);
-	
+
 	olddb=db;
 	oldaggr=aggr;
-	
+
+	fprintf(stderr, "Creating DB\n");
 	db=initEmptyDB();
 	aggr=initEmptyDB();
-	
+	fprintf(stderr,"Finished creating DB\n");
+
 	dumperJob.prefix= PREFIX "pagecounts";
 	dumperJob.db=olddb;
 	dumperJob.time=dumptime;
-	
+
 	pthread_create(&dumper,NULL,(void *)statsDumper, (void *)&dumperJob);
-	
+
 	aggrDumperJob.prefix= PREFIX "projectcounts";
 	aggrDumperJob.db = oldaggr;
 	aggrDumperJob.time=dumptime;
 
 	pthread_create(&aggrdumper,NULL,(void *)statsDumper,(void *)&aggrDumperJob);
-	
 	needdump=0;
 	alarm(PERIOD-(dumptime%PERIOD));
+	printf("ALL IS FINE\n");
+
 }
 
 /* TCP connection handling logic - unsafe dump of data in DB - should be avoided at large datasets */
@@ -264,9 +306,9 @@ void handleConnection(int c) {
 	FILE *tmp;
 	char buf[1024];
 	int r;
-	
+
 	shutdown(c,SHUT_RD);
-	
+
 	tmp=tmpfile();
 	dumpData(tmp,db);
 	rewind(tmp);
@@ -276,7 +318,7 @@ void handleConnection(int c) {
 	} else {
 		while(!feof(tmp)) {
 			r=fread((char *)&buf,1,1024,tmp);
-			write(c,(char *)&buf,r);
+			(void)write(c,(char *)&buf,r);
 		}
 		close(c);
 		fclose(tmp);
