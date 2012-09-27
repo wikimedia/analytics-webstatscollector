3b2d8dda92cc6f4afe7688055c6ef4914b13d8e7
Diederik van Liere <dvanliere@wikimedia.org>
2012-09-27 14:39:11 +0000

    Added automagically build numbering using git
    Change-Id: I47da0837ff392317201226bd3b8cf85ce44ae97d

--- debianize.sh
+++ debianize.sh
@@ -1,40 +1,36 @@
-#rm ../webstatscollector_0*
-#rm -rf ../webstatscollector-0.2
-#mkdir ../webstatscollector-0.2
-#cd ../webstatscollector-0.2
+#/bin/bash
 
-VERSION=0.2
+export DEBFULLNAME="Diederik van Liere"
+
+VERSION=`git describe | awk -F'-g[0-9a-fA-F]+' '{print $1}'`
+MAIN_VERSION=`git describe --abbrev=0`
 
 tar -cvf webstatscollector.tar --exclude-from=exclude .
 mv webstatscollector.tar ../
 cd ..
-rm -rf webstatscollector-${VERSION}
-#cd .. && 
-mkdir webstatscollector-${VERSION}
-#cd .. && 
-tar -C webstatscollector-${VERSION} -xvf webstatscollector.tar
+rm -rf webstatscollector-${MAIN_VERSION}
+mkdir webstatscollector-${MAIN_VERSION}
+tar -C webstatscollector-${MAIN_VERSION} -xvf webstatscollector.tar
 
 
-#webstatscollector_0.2.orig.tar.gz
-#cd .. && 
-rm webstatscollector-${VERSION}.orig.tar.gz
+rm webstatscollector-${MAIN_VERSION}.orig.tar.gz
 
-#cp -r webstatscollector webstatscollector_${VERSION}
-cd webstatscollector-${VERSION} 
-dh_make -c gpl2 -e dvanliere@wikimedia.org -s --createorig
-#cd ../webstatscollector-${VERSION}/debian && 
+cd webstatscollector-${MAIN_VERSION}
+mkdir m4
+dh_make -c gpl2 -e dvanliere@wikimedia.org -s --createorig -p webstatscollector_${VERSION}
 cd debian
 rm *ex *EX
-#cd ../webstatscollector-${VERSION}/debian && 
 rm README.Debian dirs
 #cp ../webstatscollector/debian/control debian/.
 #cp ../webstatscollector/debian/rules debian/.
 #cp ../webstatscollector/debian/copyright debian/.
 #cp ../webstatscollector/debian/changelog debian/.
 #cp ../webstatscollector/Makefile debian/.
-#cd ../webstatscollector_${VERSION} && 
+#cd ../webstatscollector_${VERSION} &&
 cd ..
-dpkg-buildpackage
+dpkg-buildpackage -v${VERSION}
 cd ..
-dpkg-deb --contents webstatscollector_${VERSION}_amd64.deb
-lintian webstatscollector_0.2_amd64.deb
+dpkg-deb --contents webstatscollector_${MAIN_VERSION}_amd64.deb
+lintian webstatscollector_${MAIN_VERSION}_amd64.deb
+mv webstatscollector_${MAIN_VERSION}_amd64.deb webstatscollector_${VERSION}_amd64.db
+
