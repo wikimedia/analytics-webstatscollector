aa2a3799f2dfd5b713691185503e66d06bcb1022
Petrea Corneliu Stefan <stefan@garage-coding.com>
2013-04-01 12:28:46 +0300

    Fixed segfault
    Change-Id: I63871716ade4cc657720ad42ef03c6fd7741fceb

--- test.sh
+++ test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
 TEST_SPACELESS_LINES=`cat entries-with-urls-with-spaces-2013-02-10.txt | ./filter | perl -MData::Dumper -ne '@f=split(/\s/,$_,4);  print if $f[3] =~ /\ /;' | wc -l`
-
 
 
 if [ $TEST_SPACELESS_LINES -eq 0 ]; then
@@ -12,4 +10,13 @@ else
 fi
 
 
-exit 0;
+trap "" SIGSEGV
+cat big-entry-1.txt | ./filter >/dev/null 2>/dev/null
+
+if [ $? -ne 139 ]; then
+  echo "Test2: Big fields in filter PASSED";
+else
+  echo "Test2: Big fields in filter FAILED";
+fi
+
+
