dc5020d96f28126a83e9aeca20ea97280443c9aa
Christian Aistleitner <christian@quelltextlich.at>
2013-12-02 13:54:56 +0100

    Whitespace cleanup in filter.c
    Change-Id: Ie636e5bf1265486e54e5fe02d73153293ebc9b77

--- filter.c
+++ filter.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-/* 
+/*
 
-Modified on Jun 4, 2010 by hcatlin to 
+Modified on Jun 4, 2010 by hcatlin to
 now process en.m.wikipedia.org requests into
 the mobile project, with the title being the
 language code.
@@ -26,14 +26,14 @@ char *_sep, *_lasttok, *_firsttok;
 char *wmwhitelist[] = {"commons","meta","incubator","species","strategy","outreach","usability","quality"};
 bool check_wikimedia(char *language) {
 	char **p=wmwhitelist;
-	for(;*p;p++) {
-		if(!strcmp(*p,language))
+	for (;*p;p++) {
+		if (!strcmp(*p,language))
 			return true;
 	}
 	return false;
 }
 
-/* IP addresses from which duplicate requests originate 
+/* IP addresses from which duplicate requests originate
 * Correspondence with Mark Bergsma on IRC.
 * diederik: what is exactly the issue?
 * mark: squids requesting from eachother
@@ -55,7 +55,7 @@ char *dupes[] = {"208.80.152.",
 bool check_ip(char *ip) {
 	char **prefix=dupes;
 	for (;*prefix;prefix++) {
-		if(!strncmp(*prefix,ip,strlen(*prefix)))
+		if (!strncmp(*prefix,ip,strlen(*prefix)))
 			return false;
 	}
 	return true;
@@ -76,9 +76,9 @@ const struct project {
 		{"wikiversity",".v",NULL},
 		{"wikiquote",".q",NULL},
 		{"m.wikipedia", ".mw", NULL},
-            {"wikivoyage", ".voy", NULL},
-            {"wikimediafoundation", ".f", NULL},
-            {"wikidata", ".wd", NULL},
+		{"wikivoyage", ".voy", NULL},
+		{"wikimediafoundation", ".f", NULL},
+		{"wikidata", ".wd", NULL},
 		NULL
 	}, *project;
 
@@ -98,11 +98,11 @@ void replace_space(char *url) {
 	}
 
 	int i;
-	for(i = 0; i < len; i++){
-		if(url[i] == ' '){
+	for (i = 0; i < len; i++){
+		if (url[i] == ' '){
 			url[i] = '_';
-	        }
-   	 }
+		}
+	}
 }
 
 bool parse_url(char *url, struct info *in) {
@@ -124,31 +124,30 @@ bool parse_url(char *url, struct info *in) {
 	in->language=HEAD;
 	in->project=FIELD;
 
-        if(!in->language ||
-           !in->project )
-          return false;
+	if (!in->language || !in->project )
+		return false;
 
 	if (!strncmp(in->title,"Special:CentralAutoLogin/", 25))
 		return false; /* Special:CentralAutoLogin/.* are no real page views.*/
 
-	if(!strcmp(in->project,"m")) {
-    	in->project = "m.wikipedia";
-    	in->title = in->language;
-    	return true;
+	if (!strcmp(in->project,"m")) {
+		in->project = "m.wikipedia";
+		in->title = in->language;
+		return true;
 	} else {
-  	if(strcmp(TAIL,"org"))
-  		return false;
-  	if (in->language && in->project)
-  		return true;
-  	else
-  		return false;
+	if (strcmp(TAIL,"org"))
+		return false;
+	if (in->language && in->project)
+		return true;
+	else
+		return false;
   }
 }
 
 bool check_project(struct info *in) {
 	const struct project *pr=projects;
-	for(;pr->full;pr++) {
-		if(!strcmp(in->project,pr->full)) {
+	for (;pr->full;pr++) {
+		if (!strcmp(in->project,pr->full)) {
 			in->suffix=pr->suffix;
 			/* Project found, check if filter needed */
 			if (pr->filter)
@@ -171,10 +170,10 @@ int main(int ac, char **av) {
 	setuid(65534);
 
 	char *undef,*ip,*url, *size;
-	while(fgets(line,LINESIZE-1,stdin)) {
+	while (fgets(line,LINESIZE-1,stdin)) {
 		bzero(&info,sizeof(info));
 		/* Tokenize the log line */
-		TOKENIZE(line,"\t"); /* server */ 
+		TOKENIZE(line,"\t"); /* server */
 				FIELD; /* id? */
 				FIELD; /* timestamp */
 				FIELD; /* ??? */
@@ -183,9 +182,7 @@ int main(int ac, char **av) {
 		info.size=	FIELD; /* object size */
 				FIELD;
 		url=		FIELD;
-		if(!url       ||
-		   !info.ip   ||
-		   !info.size   )
+		if (!url || !info.ip || !info.size)
 			continue;
 		replace_space(url);
 		if (!check_ip(info.ip))
@@ -197,4 +194,3 @@ int main(int ac, char **av) {
 		printf("%s%s 1 %s %s\n",info.language, info.suffix, info.size, info.title);
 	}
 }
-
